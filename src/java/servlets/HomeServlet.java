1887af29e51947fc0263d8d70d708f5df5f00567
user <user@DESKTOP-FLHNA44.cg.shawcable.net>
2021-10-17 01:31:32 -0600

    Final ver.

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -6,20 +6,35 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-    }
 
+        HttpSession session = request.getSession();
+        
+        if (session.getAttribute("username") == null) {
+            response.sendRedirect("login");
+            return;
+        }
+        
+        if (session.getAttribute("username") != null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+            return;
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        return;
+
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
 }
--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -16,6 +16,22 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        if (request.getParameter("logout") != null) {
+            request.setAttribute("message", "You have successfully logged out.");
+            session.invalidate();
+
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+
+        if (session.getAttribute("username") != null) {
+            response.sendRedirect("home");
+            return;
+        }
+
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
         return;
     }
@@ -27,6 +43,9 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        request.setAttribute("username", username);
+        request.setAttribute("password", password);
+
         if (username == null || username.equals("") || password == null || password.equals("")) {
 
             request.setAttribute("message", "Invalid login");
@@ -35,11 +54,19 @@ public class LoginServlet extends HttpServlet {
         }
 
         User user = login(username, password);
-        if (user != null) {
+        if (user == null) {
+
+            request.setAttribute("message", "Invalid login");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+
+        } else {
+
             HttpSession session = request.getSession();
             session.setAttribute("username", user.getUsername());
             response.sendRedirect("home");
             return;
+
         }
 
     }
